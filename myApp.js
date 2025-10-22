71ecc7ac48c1fd83ed1e242751c2e4c975fffe61
harsh sutariya <sutariyaharsh188@gmail.com>
2025-10-22 00:31:10 -0400

    Update README.md

--- myApp.js
+++ myApp.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const helmet = require('helmet');
+
 const app = express();
 
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+  console.log(`⏣ Hex Info Sec App Server Started on Port ${PORT}`);
+});
 
 
 
